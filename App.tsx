e01392fe1d391013a57bd7b8a884b08d74cc9045
Brian Sztamfater <bsztamfater@makingsense.com>
2021-01-01 22:27:58 -0300

    Just copy pasting react-native-walletconnect example for testing

--- App.tsx
+++ App.tsx
@@ -1,21 +1,59 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, SafeAreaView, StyleSheet } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import WalletConnectProvider, { useWalletConnect } from 'react-native-walletconnect';
+
+const WalletConnectExample = () => {
+  const {
+    createSession,
+    killSession,
+    session,
+    signTransaction,
+  } = useWalletConnect();
+  const hasWallet = !!session.length;
+  return (
+    <>
+      {!hasWallet && (
+        <Button title="Connect" onPress={createSession} />
+      )}
+      {!!hasWallet && (
+        <Button
+          title="Sign Transaction"
+          onPress={() => signTransaction({
+            from: "0xbc28Ea04101F03aA7a94C1379bc3AB32E65e62d3",
+            to: "0x89D24A7b4cCB1b6fAA2625Fe562bDd9A23260359",
+            data: "0x",
+            gasPrice: "0x02540be400",
+            gas: "0x9c40",
+            value: "0x00", 
+            nonce: "0x0114",
+          })}
+        />
+      )}
+      {!!hasWallet && (
+        <Button
+          title="Disconnect"
+          onPress={killSession}
+        />
+      )}
+    </>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WalletConnectProvider>
+      <SafeAreaView style={styles.container}>
+        <StatusBar style="auto" />
+        <WalletConnectExample />
+      </SafeAreaView>
+    </WalletConnectProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
 });
